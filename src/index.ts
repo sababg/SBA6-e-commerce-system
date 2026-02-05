a20e3882c54e3a8da3d8782eae22e8775696cc3e
sababg <beigisaba@gmail.com>
2026-02-05 01:02:32 -0500

    add utility functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import { Product } from "./models/Product.js";
 import { getProduct } from "./services/apiService.js";
-// import { calculateDiscount } from "./utils/discountCalculator.js";
-// import { calculateTax } from "./utils/taxCalculator.js";
+import { calculateDiscount } from "./utils/discountCalculator.js";
+import { calculateTax } from "./utils/taxCalculator.js";
 
 const getData = async () => {
   try {
     const data = await getProduct();
-
     const products = data.map(
       (p: any) =>
         new Product(
           p.id,
           p.title,
+          p.description,
           p.category,
           p.discountPercentage,
           p.rating,
           p.stock,
-          p.description,
           p.price,
         ),
     );
+
+    console.log("products", products);
     products.map((element) => {
-      return element.displayDetails();
+      element.displayDetails();
+      calculateDiscount(element.price, element.discountPercentage);
+      calculateTax(element.price, element.category);
     });
-    products.map((element) => console.log(element.displayDetails())); // To see the result
+
+    products.map((element) => {
+      console.log(element.displayDetails());
+      console.log(calculateDiscount(element.price, element.discountPercentage));
+      console.log(calculateTax(element.price, element.category));
+    }); // To see the result
   } catch (error) {
     console.error(error);
   }
